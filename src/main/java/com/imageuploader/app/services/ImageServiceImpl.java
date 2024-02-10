4e2e5269f4d308f35798f639f2ac8eab8b891549
Luis Gutiérrez <luisghtz@outlook.com>
2024-02-10 14:18:23 -0600

    Handle response error.

--- src/main/java/com/imageuploader/app/services/ImageServiceImpl.java
+++ src/main/java/com/imageuploader/app/services/ImageServiceImpl.java
@@ -4,9 +4,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Date;
 
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -17,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
-
+    private final Logger log = LoggerFactory.getLogger(ImageServiceImpl.class);
     @Value("${aws.bucketName}")
     private String bucketName;
 
@@ -26,21 +30,23 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public String uploadImage(MultipartFile image) {
         File file = new File(image.getOriginalFilename());
+        String fileName = "";
 
         try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
             fileOutputStream.write(image.getBytes());
+            fileName = generateFileName(image);
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, file);
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(image.getContentType());
+            metadata.setContentLength(file.length());
+            putObjectRequest.setMetadata(metadata);
+            putObjectRequest.setMetadata(metadata);
+            s3Client.putObject(putObjectRequest);
         } catch (Exception e) {
-            e.printStackTrace();
+            file.delete();
+            log.error("Error uploading image {}", e.getMessage());
+            throw new ResponseStatusException(HttpStatus.SC_INTERNAL_SERVER_ERROR, "Error uploading image", e);
         }
-
-        String fileName = generateFileName(image);
-        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, file);
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(image.getContentType());
-        metadata.setContentLength(file.length());
-        putObjectRequest.setMetadata(metadata);
-        putObjectRequest.setMetadata(metadata);
-        s3Client.putObject(putObjectRequest);
 
         file.delete();
 
